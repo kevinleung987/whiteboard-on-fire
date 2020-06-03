631b899591b8d07e226db94ce7524922673c1b2e
Kevin Leung <kevin.leung987@outlook.com>
2020-06-03 15:57:30 -0400

    Add Send button to Chat

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,19 +2,37 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import CanvasDraw from "react-canvas-draw";
 import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const useStyles = makeStyles((theme) => ({
   canvas: {
     width: "100%",
-    height: "100%",
+    height: "90%",
+  },
+  options: {
+    marginTop: 16,
+    width: "100%",
+    height: "10%",
   },
 }));
 
 export default function Canvas() {
   const classes = useStyles();
   return (
-    <Paper className={classes.canvas} elevation={3}>
-      <CanvasDraw canvasWidth={"100%"} canvasHeight={"100%"} />
-    </Paper>
+    <>
+      <Paper className={classes.canvas} elevation={3}>
+        <CanvasDraw canvasWidth={"100%"} canvasHeight={"100%"} />
+      </Paper>
+      <Paper className={classes.options} elevation={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={2}>
+            Red
+          </Grid>
+          <Grid item xs={2}>
+            Blue
+          </Grid>
+        </Grid>
+      </Paper>
+    </>
   );
 }
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,53 +1,105 @@
-import React from "react";
+import {
+  Grid,
+  Avatar,
+  Button,
+  Input,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import { useUser, useDatabase, useDatabaseList } from "reactfire";
-import Input from "@material-ui/core/Input";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import React from "react";
+import { useDatabase, useDatabaseList, useUser } from "reactfire";
 
 const useStyles = makeStyles((theme) => ({
   chat: {
     height: "60vh",
+    overflow: "auto",
   },
   sendMessage: { marginTop: 16, padding: 8 },
 }));
 
 export default function Chat() {
-  const handleChange = async (event) => {
-    event.preventDefault();
-    const value = event.target.value;
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  }
+  const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-      await ref.push({
-        content: message,
-        timestamp: Date.now(),
-        author: user.displayName,
-      });
-    } else {
-      setMessage(value);
+      sendMessage(event.target.value);
     }
   };
 
+  const sendMessage = (message) => {
+    ref.push({
+      content: message,
+      timestamp: Date.now(),
+      author: user.displayName,
+    });
+    setMessage('');
+  };
+
   const classes = useStyles();
+  const messagesBottomRef = React.useRef();
   const user = useUser();
   const db = useDatabase();
   const ref = db.ref("boards/example/messages");
   const changes = useDatabaseList(ref);
   const [message, setMessage] = React.useState("");
+  React.useEffect(
+    () => messagesBottomRef.current.scrollIntoView({ behavior: "smooth" }),
+    [changes]
+  );
 
   return (
     <div>
       <Paper className={classes.chat} elevation={3}>
-        {changes.map(({ snapshot }) => {
-          const { content, timestamp, author } = snapshot.val();
+        <List>
+          {changes.map(({ snapshot }) => {
+            const { content, timestamp, author } = snapshot.val();
+            const date = new Date(timestamp).toLocaleTimeString();
 
-          return (
-            <p key={snapshot.key}>
-              <b>{author}</b>: {content}
-            </p>
-          );
-        })}
+            return (
+              <ListItem key={snapshot.key}>
+                <ListItemAvatar>
+                  <Avatar>
+                    <AccountCircleIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primaryTypographyProps={{ noWrap: true }}
+                  primary={content}
+                  secondary={`Sent by ${author} at ${date}`}
+                />
+              </ListItem>
+            );
+          })}
+          <div ref={messagesBottomRef} />
+        </List>
       </Paper>
       <Paper className={classes.sendMessage}>
-        <Input placeholder="Send a Message" fullWidth onKeyUp={handleChange} />
+        <Grid container>
+          <Grid item xs={10}>
+            <Input
+              placeholder="Send a Message"
+              fullWidth
+              value={message}
+              onKeyDown={handleKeyPress}
+              onChange={handleChange}
+            />
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              color="primary"
+              disabled={message.length === 0}
+              onClick={() => sendMessage(message)}
+            >
+              Send
+            </Button>
+          </Grid>
+        </Grid>
       </Paper>
     </div>
   );
