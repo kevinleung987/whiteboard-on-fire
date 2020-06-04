9d71a0383de1a21b1b3d28f9c5b2281ebe80eeb4
Kevin Leung <kevin.leung987@outlook.com>
2020-06-04 17:05:26 -0400

    Enable Database emulator

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,7 +1,13 @@
+const databaseURL =
+  // eslint-disable-next-line no-restricted-globals
+  location.hostname === "localhost"
+    ? "http://localhost:9000/?ns=fir-firstweek"
+    : "https://fir-firstweek.firebaseio.com";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyCbWB8Vf9cX4tHozXCVFTj2eH2K9nBYEs0",
   authDomain: "fir-firstweek.firebaseapp.com",
-  databaseURL: "https://fir-firstweek.firebaseio.com",
+  databaseURL,
   projectId: "fir-firstweek",
   storageBucket: "fir-firstweek.appspot.com",
   messagingSenderId: "940617187800",
