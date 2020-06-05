326de0526e01ecae9d5355e8dc5e023fe58d5594
Kevin Leung <kevin.leung987@outlook.com>
2020-06-05 19:55:28 -0400

    Add cleanup for RTDB via Cloud Function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,37 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Clean up Board if there are no users present
+exports.cleanup = functions.database
+  .ref("status/")
+  .onDelete(async (snapshot, context) => {
+    const original = snapshot.val();
+    const originalBoard = original[Object.keys(original)[0]]["board"];
+    await admin
+      .database()
+      .ref("status/")
+      .once(
+        "value",
+        (snapshot) => {
+          if (snapshot.val()) {
+            const value = snapshot.val();
+            const allKeys = Object.keys(value);
+            allKeys.forEach((key) => {
+              const { board } = value[key];
+              if (board === originalBoard) {
+                return false;
+              }
+            });
+            admin.database().ref(`boards/${originalBoard}`).remove();
+            console.log(`Removed ${originalBoard}`);
+            return true;
+          } else {
+            admin.database().ref(`boards/${originalBoard}`).remove();
+            console.log(`Removed ${originalBoard}`);
+          }
+        },
+        (error) => console.log("Failure", error.code)
+      );
+    return true;
+  });
